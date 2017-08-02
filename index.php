b6995f459b3ab4a9fe34876e0b63115804787b62
Pavan Jadhao <jadhao.pavan643@outlook.com>
2017-08-02 18:53:16 +0530

    File Remove Function

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php
-phpinfo();
-echo "Hello world";
-echo "Changes Commited Successfully."
-?>
+    $file = "note1.txt";
+
+    if(file_exists($file)) {
+        if(unlink($file)) {
+            echo "File removed successfully.";
+        } else {
+            echo "File not removed.";
+        }
+    } else {
+        echo "file not exist, we created new file.";
+        fopen("new.txt","w");
+    }
