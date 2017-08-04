81fa57792219c06af86ef90b609a25de8691b089
Pavan Jadhao <jadhao.pavan643@outlook.com>
2017-08-04 08:04:27 +0530

    Copying file to another location

--- index.php
+++ index.php
@@ -1,13 +1,45 @@
-<?php
-    $file = "note1.txt";
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>PHP Program Practice.</title>
 
-    if(file_exists($file)) {
-        if(unlink($file)) {
-            echo "File removed successfully.";
-        } else {
-            echo "File not removed.";
-        }
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+
+<?php
+    $file = 'new.txt';
+    $newfile = 'test/new.txt';
+
+//Check existence of file
+if (file_exists($file)) {
+    if (copy($file, $newfile)) {
+        echo '<div class="alert alert-success" role="alert">File Copied Successfully.</div>';
     } else {
-        echo "file not exist, we created new file.";
-        fopen("new.txt","w");
+        echo '<div class="alert alert-danger" role="alert">ERROR : Coping File.</div>';
     }
+} else {
+    echo '<div class="alert alert-danger" role="alert">File Not Exist.</div>';
+}
+?>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+  </body>
+</html>
