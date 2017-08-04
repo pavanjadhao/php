7991a5cea6dfc8053bdf71d4c2f2dfdc338ad11c
Pavan Jadhao <jadhao.pavan643@outlook.com>
2017-08-04 09:30:30 +0530

    Scan Dir using glob function

--- index.php
+++ index.php
@@ -21,10 +21,32 @@
   </head>
   <body>
 <?php
-//Scan files of specified extension
-    foreach(glob("Car/AC-DC/*.mp4") as $file) {
-        echo basename($file) . '  Size : ' . filesize($file)/1024/1024 . '(byte)' . '<br><hr>';
+// Define a function to output files in a directory
+function outputFiles($path){
+    // Check directory exists or not
+    if(file_exists($path) && is_dir($path)){
+        // Search the files in this directory
+        $files = glob($path ."/*");
+        if(count($files) > 0){
+            // Loop through retuned array
+            foreach($files as $file){
+                if(is_file("$file")){
+                    // Display only filename
+                    echo basename($file) . "<br>";
+                } else if(is_dir("$file")){
+                    // Recursively call the function if directories found
+                    outputFiles("$file");
+                }
+            }
+        } else{
+            echo "ERROR: No such file found in the directory.";
+        }
+    } else {
+        echo "ERROR: The directory does not exist.";
     }
+}
+// Call the function
+outputFiles("Car");
 ?>
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
