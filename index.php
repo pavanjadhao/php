64d44d7e0bb669f682e67f512db93c90765e86b2
Pavan Jadhao <jadhao.pavan643@outlook.com>
2017-08-06 12:35:55 +0530

    Sanitization and Validation

--- index.php
+++ index.php
@@ -1,36 +1,79 @@
 <?php
-include_once __DIR__ . '/vendor/autoload.php';
-// use PHPMailer;
-$mail = new PHPMailer;
+    
+    //validate integer range
+    $int = 75;
+    // Validate sample integer value
+    if(filter_var($int, FILTER_VALIDATE_INT,
+    array("options" => array("min_range" => 0,"max_range" => 100)))){
+        echo "The <b>$int</b> is within the range of 0 to 100";
+    } else {
+        echo "The <b>$int</b> is not within the range of 0 to 100";
+    }
 
-//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+    // Sample website url
+    $url = "http://www.example.com";
+    
+    // Validate website url for query string
+    if(filter_var($url, FILTER_VALIDATE_URL, FILTER_FLAG_QUERY_REQUIRED)){
+        echo "The <b>$url</b> contains query string";
+    } else{
+        echo "The <b>$url</b> does not contain query string";
+    }
 
-$mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
-$mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'jadhao.pavan643@gmail.com';                 // SMTP username
-$mail->Password = 'pj72826@ghostman';                           // SMTP password
-$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 587;                                    // TCP port to connect to
+    // Sample website url
+    $url = "http://www.example.com";
+    
+    // Remove all illegal characters from url
+    $url = filter_var($url, FILTER_SANITIZE_URL);
+    
+    // Validate website url
+    if(filter_var($url, FILTER_VALIDATE_URL)){
+        echo "The <b>$url</b> is a valid website url";
+    } else{
+        echo "The <b>$url</b> is not a valid website url";
+    }
 
-$mail->setFrom('jadhao.pavan643@gmail.com', 'Pavan Jadhao');
-$mail->addAddress('jadhao.pavan@outlook.com', 'Pavan Jadhao');     // Add a recipient
-// $mail->addAddress('ellen@example.com');               // Name is optional
-// $mail->addReplyTo('info@example.com', 'Information');
-// $mail->addCC('cc@example.com');
-// $mail->addBCC('bcc@example.com');
+    //sanitizing and validating email address
+    $emailInput = "pavan.jadhao@gmail.com";
+    //sanitizing email
+    $email = filter_var($emailInput,FILTER_SANITIZE_EMAIL);
+    //validating email
+    if(filter_var($email,FILTER_VALIDATE_EMAIL)) {
+        echo '<b>' . $emailInput . '</b>' . ' valid email.';
+    }else {
+        echo '<b>' . $emailInput . '</b>' . ' not valid email.'; 
+    }
 
-// $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-// $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-// $mail->isHTML(true);                                  // Set email format to HTML
+    //Check the IP address is v4 or v6
+    $IP = "2001:db8:85a3:8d3:1319:8a2e:370:7348";
+    if(filter_var($IP,FILTER_VALIDATE_IP,FILTER_FLAG_IPV4)) {
+        echo 'IPV4';
+    }elseif(filter_var($IP,FILTER_VALIDATE_IP,FILTER_FLAG_IPV6)) {
+        echo 'IPV6';
+    }else {
+        echo 'This is not IP address.';
+    }
 
-$mail->Subject = 'Test';
-$mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+    $inIp = '255.255.255.255';
+    //By default this validation assumes that 0 is not valid integer
+    if(filter_var($inIp,FILTER_VALIDATE_IP)){ //We can also validate IPV4 or IPV6
+        echo '<b style="color:green">' . $inIp. " is valid IP address </b>";
+    }else {
+        echo '<b style="color:red">' . $inIp  . " is not valid IP address </b>";
+    }
 
-if(!$mail->send()) {
-    echo 'Message could not be sent.';
-    echo 'Mailer Error: ' . $mail->ErrorInfo;
-} else {
-    echo 'Message has been sent';
-}
+    ///Sanitizing String
+    $input = "<p>This Is Simple Sanitization</p><br><h1> This Is Simple Sanitization</h1>";
+    $sanitizedInput = filter_var($input,FILTER_SANITIZE_STRING);
+    echo $sanitizedInput;
+
+    //Validathing Integer
+    $inNumber = 54564156;
+    //By default this validation assumes that 0 is not valid integer
+    if(filter_var($inNumber,FILTER_VALIDATE_INT) == 0 || filter_var($inNumber,FILTER_VALIDATE_INT)){
+        echo '<b style="color:green">' . $inNumber. " is valid integer </b>";
+    }else {
+        echo '<b style="color:red">' . $inNumber  . " is not valid integer </b>";
+    }
+
+
