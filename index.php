88cc96aa62aeca73b3a43097ee9ddf40d548953e
Pavan Jadhao <jadhao.pavan643@outlook.com>
2017-08-07 15:59:45 +0530

    Ways of coonecting to msql database

--- index.php
+++ index.php
@@ -1,19 +1,43 @@
-<?php
-function handleUncaughtException($e){
-    // Display generic error message to the user
-    echo "Opps! Something went wrong. Please try again, or contact us if the problem persists.";
-    
-    // Construct the error string
-    $error = "Uncaught Exception: " . $message = date("Y-m-d H:i:s - ");
-    $error .= $e->getMessage() . " in file " . $e->getFile() . " on line " . $e->getLine() . "\n";
-    
-    // Log details of error in a file
-    error_log($error, 3, "logs/app_errors.log");
+  <?php
+//procedural way of connecting to the database server
+$link = mysqli_connect('localhost','root','');
+
+//cheking for connection
+if($link == false) {
+    die("Error : could not connect." . mysqli_connect_errno());
 }
- 
-// Register custom exception handler
-set_exception_handler("handleUncaughtException");
- 
-// Throw an exception
-throw new Exception("Testing Exception!");
-?>
+
+//printing host information
+echo "Connect Successfull. Host info : " . mysqli_get_host_info($link);
+//closing database connection
+mysqli_close($link);
+
+//Object oriented way of connecting to database
+$mysqli = new mysqli('localhost','root',"","practice");
+
+//checking for connection
+if($mysqli === false){
+    die("Error : Could not connect." . $mysqli -> connect_error);
+}
+
+//printing host information
+echo "Connect Successfully." . $mysqli -> host_info;
+//closing databse connection
+$mysqli -> close();  
+
+
+//another way of connecting to database is PDO(PHP Data Object)
+try {
+    $pdo = new PDO('mysql:host=localhost','root','');
+
+    //Setting PDO error mode to exception
+    $pdo -> setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+    //print host information
+    echo 'Connect Successfully. Host info : ' . $pdo -> getAttribute(constant('PDO::ATTR_CONNECTION_STATUS'));
+}catch(PDOException $e) {
+    die('Could not connect.' . $e -> getMessage());
+}
+
+//closing connection to the database
+unset($pdo);
